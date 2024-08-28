2b8a7136d8fdcdce13965a918a16130ddf688edf
Henry Yeung <blackcityhenry@gmail.com>
2024-08-28 09:40:09 +0800

    ADD: mason options

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,20 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "css-lsp",
+      "markdown-toc",
+      "stylua",
+      "emmet-language-server",
+      "marksman",
+      "typescript-language-server",
+      "json-lsp",
+      "prettier",
+      "lua-language-server",
+      "shfmt",
+      "vue-language-server",
+      "markdownlint-cli2",
+      "some-sass-language-server",
+    },
+  },
+}
